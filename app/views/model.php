ee1ee2d58fe281ac36fc982f940c11dfeeb519bf
nampoinafandresena <razafindraibenampoina98@gmail.com>
2026-02-16 21:31:07 +0300

    le / tao am le assets miantso CSS no tsy nety teo

--- app/views/model.php
+++ app/views/model.php
@@ -7,7 +7,7 @@
     
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Playfair+Display:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css">
 </head>
 <body>
 
