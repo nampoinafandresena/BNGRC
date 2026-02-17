f03cf03d433ea93da5052568bf6a8a182d55026a
anjara <loranasl17@gmail.com>
2026-02-17 10:51:16 +0300

    modif route

--- index.php
+++ index.php
@@ -35,4 +35,4 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $ds = DIRECTORY_SEPARATOR;
 
-require(__DIR__. $ds . 'app' . $ds . 'config' . $ds . 'bootstrap.php');
+require(__DIR__ . $ds . 'app' . $ds . 'config' . $ds . 'bootstrap.php');
